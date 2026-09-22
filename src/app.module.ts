76cf38985bc48681e8790304ff82f6b84b8a786c
Everilll <averil_mauladani_33rpl@student.smktelkom-mlg.sch.id>
2026-09-22 15:02:47 +0700

    feat: integrate common structure using npx @averildwi/nest-common

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { PrismaModule } from './prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppConfigModule } from './common/config/app-config.module';
+import { HashingModule } from './common/hashing/hashing.module';
 
 @Module({
   imports: [
+    AppConfigModule.forProject(),
+    HashingModule,
     PrismaModule,],
   controllers: [AppController],
   providers: [AppService],
